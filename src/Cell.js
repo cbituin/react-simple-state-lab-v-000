cc68c9e0831d6ebadeca21374a5a3398fc042116
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2020-02-27 18:37:03 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,11 +1,52 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
   
+=======
+<<<<<<< HEAD
+import React, { Component } from "react";
+
+export default class Cell extends Component {
+=======
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  
+>>>>>>> origin/solution
+>>>>>>> d4aec8ad7ef0e62c580d68bac9b31c3f1d143088
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.value
+<<<<<<< HEAD
+    }
+=======
+<<<<<<< HEAD
+    };
+>>>>>>> d4aec8ad7ef0e62c580d68bac9b31c3f1d143088
+  }
+  
+  handleClick = () => {
+    this.setState({
+      color: '#333'
+    })
+  }
+  
+  render() {
+    return (
+      <div 
+        className="cell"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}>
+      </div>
+    )
+  }
+<<<<<<< HEAD
+  
+}
+=======
+=======
     }
   }
   
@@ -25,4 +66,6 @@ export default class Cell extends Component {
     )
   }
   
+>>>>>>> origin/solution
 }
+>>>>>>> d4aec8ad7ef0e62c580d68bac9b31c3f1d143088
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,7 +1,29 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import Cell from './Cell.js'
 
 export default class Matrix extends Component {
+=======
+<<<<<<< HEAD
+import React, { Component } from "react";
+import Cell from "./Cell";
+
+export default class Matrix extends Component {
+  // Maps array => ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+  genRow = vals => vals.map(data => <Cell value={data} />);
+  genMatrix = () =>
+    this.props.values.map(
+      rowVals => <div className="row">{this.genRow(rowVals)}</div>
+      //returns 10x of
+      //= > ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+    );
+
+=======
+import React, { Component } from 'react';
+import Cell from './Cell.js'
+
+export default class Matrix extends Component {
+>>>>>>> d4aec8ad7ef0e62c580d68bac9b31c3f1d143088
   
   genRow = (vals) => (
     vals.map(val => <Cell value={val} />)
@@ -11,6 +33,10 @@ export default class Matrix extends Component {
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
   
+<<<<<<< HEAD
+=======
+>>>>>>> origin/solution
+>>>>>>> d4aec8ad7ef0e62c580d68bac9b31c3f1d143088
   render() {
     return (
       <div id="matrix">
@@ -26,4 +52,15 @@ Matrix.defaultProps = {
     const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
     return (new Array(10).fill(defRow))
   })()
+<<<<<<< HEAD
 }
+=======
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
+}
+>>>>>>> d4aec8ad7ef0e62c580d68bac9b31c3f1d143088
